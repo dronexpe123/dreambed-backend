10a3d5b8bd22d5309aee765db6deca0204f438df
dronexpe123 <dronexperience123@gmail.com>
2026-04-29 00:49:07 -0400

    agregar previuw

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ CORS(app, resources={
         "origins": [
             "https://dream-bed.netlify.app",
             "http://localhost",
-            "http://127.0.0.1:5500",
+            "http://127.0.0.1:5500/dreambed-fronted/index.html",
             "null",
         ],
         "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
